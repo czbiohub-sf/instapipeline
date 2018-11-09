d99bed653c533a7f255a774904f4e9cae2621621
JennyVoPhamhi <jenny.vophamhi@czbiohub.org>
2018-11-09 11:50:42 -0800

    Moved size and clumpiness sorting methods from SAA class to util.py

--- Annotation/util.py
+++ Annotation/util.py
@@ -161,6 +161,10 @@ def csv_to_kdt(csv_filepath, img_height):
 	ref_kdt = KDTree(ref_points, leaf_size=2, metric='euclidean')	# kdt is a kd tree with all the reference points
 	return ref_kdt
 
+"""
+Pair scores
+"""
+
 def get_pair_scores(df):
 	""" Calculate pair scores for each pair of workers in df.
 
@@ -263,3 +267,206 @@ def slice_by_worker_pair_score(df, threshold):
 	for worker in high_scoring_workers:
 		df = df[df.worker_id != worker]
 	return df
+
+"""
+Sorting clusters by size and clumpiness
+"""
+
+def get_cluster_size_threshold(clusters):
+	""" Calculate a cluster size threshold for all clusters
+	using K-means in 1D. Assumes a bimodal distribution.
+
+	Parameters
+	----------
+	clusters : pandas dataframe 
+		(centroid_x | centroid_y | members)
+
+	Returns
+	-------
+	cluster size threshold
+	"""
+	total_list = []
+	for i in range(len(clusters.index)):
+		row = clusters.iloc[[i]]
+		members = row.iloc[0]['members']
+		worker_list = [member[3] for member in members]
+		num_members = len(np.unique(worker_list))
+		total_list.append(num_members)
+	total_array = np.asarray(total_list)
+	km = KMeans(n_clusters = 2).fit(total_array.reshape(-1,1))
+	cluster_centers = km.cluster_centers_
+	return (cluster_centers[0][0]+cluster_centers[1][0])/2
+
+def sort_clusters_by_size(clusters, threshold):
+	""" Sort clusters by quantity of unique annotators.
+
+	Parameters
+	----------
+	clusters : pandas dataframe 
+		(centroid_x | centroid_y | members)
+	threshold : threshold quantity of unique annotators
+
+	Returns
+	-------
+	small_clusters : pandas dataframe containing clusters 
+		for which num unique annotators < threshold
+		(centroid_x | centroid_y | members)
+	large_clusters : pandas dataframe containing clusters 
+		for which num unique annotators >= threshold
+		(centroid_x | centroid_y | members)
+	"""
+	small_clusters_list = []
+	large_clusters_list = []
+	for i in range(len(clusters.index)):
+		row = clusters.iloc[[i]]
+		members = row.iloc[0]['members']
+		centroid_x = row.iloc[0]['centroid_x']
+		centroid_y = row.iloc[0]['centroid_y']
+
+		worker_list = []
+		for member in members:
+			worker_list.append(member[3])
+		num_members = len(np.unique(worker_list))
+
+		if (num_members < threshold):
+			small_clusters_list.append([centroid_x, centroid_y, members])
+		else:
+			large_clusters_list.append([centroid_x, centroid_y, members])
+
+	small_clusters = pd.DataFrame(index = range(len(small_clusters_list)), columns = ['centroid_x','centroid_y','members'])
+	large_clusters = pd.DataFrame(index = range(len(large_clusters_list)), columns = ['centroid_x','centroid_y','members'])
+
+	for i in range(len(small_clusters_list)):
+		small_clusters['centroid_x'][i] = small_clusters_list[i][0]
+		small_clusters['centroid_y'][i] = small_clusters_list[i][1]
+		small_clusters['members'][i] = small_clusters_list[i][2]
+
+	for i in range(len(large_clusters_list)):
+		large_clusters['centroid_x'][i] = large_clusters_list[i][0]
+		large_clusters['centroid_y'][i] = large_clusters_list[i][1]
+		large_clusters['members'][i] = large_clusters_list[i][2]
+
+	return small_clusters, large_clusters
+
+def get_clumpiness_threshold(clusters, bin_size, cutoff_fraction):
+	""" Calculate a clumpiness threshold for all clusters
+	by finding the value between the tail and the main mode. 
+	Assumes a left-skewed unimodal distribution.
+
+	Protocol for finding threshold:
+	Sort all clusters into bins.
+		e.g. if bin_size = 0.1, then sort clusters into bins 100-95%, 95-85%, ..., 5-0% 
+		(% of contributors contributed only once to this cluster)
+	Find all values between two adjacent bins where the number of clusters in the higher-value 
+		bin is at least cutoff_fraction times greater than the number of clusters in the lower-value bin, 
+		and neither bin contains zero clusters.
+	threshold is the lowest of these values minus 0.1 (in order to move one bin to the left, 
+		to minimize the number of clusters which are actually single in the group of clusters 
+		detected as clumpy), or 0 if no such values exist.
+
+	Parameters
+	----------
+	clusters : pandas dataframe 
+		(centroid_x | centroid_y | members)
+	bin_size : see protocol
+	cutoff_fraction : see protocol
+
+	Returns
+	-------
+	clumpiness threshold
+	"""
+	single_fraction_list = []
+	for i in range(len(clusters.index)):
+		row = clusters.iloc[[i]]
+		members = row.iloc[0]['members']
+		x_coords = []
+		y_coords = []
+		workers = []
+		for member in members:
+			x_coords.append(member[0])
+			y_coords.append(member[1])
+			workers.append(member[3])
+
+		# Calculate replication of unique workers for each cluster
+		unique_workers = np.unique(workers)
+		num_instances_list = []
+		for unique_worker in unique_workers:
+			num_instances_list.append(workers.count(unique_worker))
+		singles = num_instances_list.count(1)
+		single_fraction = singles/len(unique_workers)
+		single_fraction_list.append(single_fraction)
+
+	(n, bins, patches) = plt.hist(single_fraction_list, bins=np.arange(0, 1+2*bin_size, bin_size) - bin_size/2)
+	total_counts_reversed = list(reversed(n))
+
+	threshold = 0
+	prev_count = 0
+	for i in range(len(total_counts_reversed)):
+		count = total_counts_reversed[i]
+		if (count != 0):
+			if((count < prev_count/cutoff_fraction) and (count != 0) and (prev_count != 0)):
+				threshold = 1 - i*bin_size - bin_size/2
+		prev_count = count
+	return threshold
+
+def sort_clusters_by_clumpiness(clusters, threshold):
+	""" Sort clusters by fraction of contributors who contribute once
+	to the cluster.
+
+	Parameters
+	----------
+	clusters : pandas dataframe 
+		(centroid_x | centroid_y | members)
+	threshold : threshold fraction of contributors who only contribute once
+
+	Returns
+	-------
+	clumpy_clusters : pandas dataframe containing clusters 
+		for which fraction of contributors who only contribute once < threshold
+		(centroid_x | centroid_y | members)
+	nonclumpy_clusters : pandas dataframe containing clusters 
+		for which fraction of contributors who only contribute once >= threshold
+		(centroid_x | centroid_y | members)
+	"""
+	clumpy_clusters_list = []
+	nonclumpy_clusters_list = []
+	clumpy_counter = 0
+	nonclumpy_counter = 0
+	for j in range(len(clusters.index)):
+		row = clusters.iloc[[j]]
+		members = row.iloc[0]['members']
+		centroid_x = row.iloc[0]['centroid_x']
+		centroid_y = row.iloc[0]['centroid_y']
+
+		workers = []
+		for member in members:
+			workers.append(member[3])
+		unique_workers = np.unique(workers)
+
+		num_instances_list = []
+		for unique_worker in unique_workers:
+			num_instances_list.append(workers.count(unique_worker))
+		singles = num_instances_list.count(1)
+		single_fraction = singles/len(unique_workers)
+
+		if (single_fraction < threshold):
+			clumpy_clusters_list.append([centroid_x, centroid_y, members])
+			clumpy_counter += 1
+		else:
+			nonclumpy_clusters_list.append([centroid_x, centroid_y, members])
+			nonclumpy_counter += 1
+
+	clumpy_clusters = pd.DataFrame(index = range(clumpy_counter), columns = ['centroid_x','centroid_y','members'])
+	nonclumpy_clusters = pd.DataFrame(index = range(nonclumpy_counter), columns = ['centroid_x','centroid_y','members'])
+
+	for k in range(clumpy_counter):
+		clumpy_clusters['centroid_x'][k] = clumpy_clusters_list[k][0]
+		clumpy_clusters['centroid_y'][k] = clumpy_clusters_list[k][1]
+		clumpy_clusters['members'][k] = clumpy_clusters_list[k][2]
+
+	for m in range(nonclumpy_counter):
+		nonclumpy_clusters['centroid_x'][m] = nonclumpy_clusters_list[m][0]
+		nonclumpy_clusters['centroid_y'][m] = nonclumpy_clusters_list[m][1]
+		nonclumpy_clusters['members'][m] = nonclumpy_clusters_list[m][2]
+
+	return clumpy_clusters, nonclumpy_clusters
